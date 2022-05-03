ec6ddff19206892a3908e0ac5f6902260cae1767
BrianBravoski <brianbravoski28@gmail.com>
2022-05-03 21:05:07 +0300

    abot and experience css and jsx

--- kemboi-portfolio/src/components/about/About.jsx
+++ kemboi-portfolio/src/components/about/About.jsx
@@ -1,9 +1,54 @@
 import React from 'react'
 import './about.css'
 
+import me from "../../assets/me.jpg";
+import { FaAward } from "react-icons/fa";
+import { FiUsers } from "react-icons/fi";
+import { VscFolderLibrary } from "react-icons/vsc";
+
 const About = () => {
   return (
-    <div>About</div>
+    <section id='about'>
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+           <div className="about__me-image">
+            <img src={me} alt="About" />
+          </div> 
+        </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+            <article className='about__card'>
+            <FaAward className="about__icon" />
+              <h5>Experience</h5>
+              <small>1+ Year</small>
+            </article>
+            <article className="about__card">
+              <FiUsers className="about__icon" />
+              <h5>Clients</h5>
+              <small>10+</small>
+            </article>
+            <article className="about__card">
+              <VscFolderLibrary className="about__icon" />
+              <h5>Projects</h5>
+              <small>20+ Completed</small>
+            </article>
+          </div>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero
+            laudantium cum esse. Aliquam repellendus excepturi voluptatibus
+            necessitatibus officiis soluta, fuga praesentium iste maiores
+            aperiam blanditiis at unde vero numquam veritatis?
+          </p>
+
+          <a href="#contact" className="btn btn-primary"> Let's Chat</a>          
+        </div>
+      </div>
+    </section>
   )
 }
 
--- kemboi-portfolio/src/components/experience/Experience.jsx
+++ kemboi-portfolio/src/components/experience/Experience.jsx
@@ -1,9 +1,107 @@
 import React from 'react'
 import './experience.css'
 
+// import { BsPatchCheckFill } from "react-icons/bs";
+import {FaReact, FaCss3Alt} from 'react-icons/fa'
+import {DiMongodb, DiMysql} from 'react-icons/di'
+import {AiOutlineHtml5, AiOutlineCloud} from 'react-icons/ai'
+import {SiJavascript,SiTailwindcss,SiBootstrap, SiPython, SiNodedotjs} from 'react-icons/si'
+
 const Experience = () => {
   return (
-    <div>Experience</div>
+    <section id='experience'>
+      <h5> What Skills I Have</h5>
+      <h2>My Experience</h2>
+      <div className="container experience__container">
+        <div className="experience__frontend">
+          <h3>Frontend Development</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+            <AiOutlineHtml5 className="experience__details-icon" />
+              <div>
+                <h4>HTML</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <FaCss3Alt className="experience__details-icon" />
+              <div>
+                <h4>CSS</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <SiJavascript className="experience__details-icon" />
+              <div>
+                <h4>JavaScript</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <SiTailwindcss className="experience__details-icon" />
+              <div>
+                <h4>Tailwind</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <SiBootstrap className="experience__details-icon" />
+              <div>
+                <h4>Bootstrap</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <FaReact className="experience__details-icon" />
+              <div>
+                <h4>React</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+          </div>
+        </div>
+        <div className="experience__backend">
+          <h3>Backend Development</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <DiMongodb className="experience__details-icon" />
+              <div>
+                <h4>MongoDB</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <DiMysql className="experience__details-icon" />
+              <div>
+                <h4>MySQL</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <SiPython className="experience__details-icon" />
+              <div>
+                <h4>Python</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <SiNodedotjs className="experience__details-icon" />
+              <div>
+                <h4>NodeJs</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <AiOutlineCloud className="experience__details-icon" />
+              <div>
+                <h4>Cloud</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
