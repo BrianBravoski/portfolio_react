95e56adfd11087e68ba2e744c4a0802f071533be
BrianBravoski <brianbravoski28@gmail.com>
2022-05-03 23:08:07 +0300

    footer component css and jsx

--- kemboi-portfolio/src/components/contact/Contact.jsx
+++ kemboi-portfolio/src/components/contact/Contact.jsx
@@ -1,10 +1,50 @@
 import React from 'react'
 import './contact.css'
+import {MdOutlineMail} from 'react-icons/md'
+import {RiMessengerLine} from 'react-icons/ri'
+import {BsWhatsapp} from 'react-icons/bs'
 
-const Contact = () => {
+
+const contact = () => {
   return (
-    <div>Contact</div>
+    <section id='contact'>
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+    
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className='contact__option'>
+        <MdOutlineMail className='contact__option-icon'/>
+          <h4>Email</h4>
+          <h5>brianbravoski28@gmail.com</h5>
+          <a href="mailto:brianbravoski28@gmail.com">Send a Message</a>
+          </article>
+
+          <article className='contact__option'>
+        <RiMessengerLine className='contact__option-icon'/>
+          <h4>Messenger</h4>
+          <h5>Brian Kemboi</h5>
+          <a href="https://m.me/">Send a Message</a>
+          </article>
+
+          <article className='contact__option'>
+        <BsWhatsapp className='contact__option-icon'/>
+          <h4>WhatsApp</h4>
+          <h5>Text Me </h5>
+          <a href="https://whatsapp.com/send?phone=+254">Send a Message</a>
+          </article>
+        </div>
+
+
+        <form action="" method="post">
+          <input type="text" name='name' placeholder='Your Full Name' required />
+          <input type="email" name="email" placeholder='Your email'required />
+          <textarea name="message"  rows="7" placeholder='Your Message' required></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
+      </div>
+    </section>
   )
 }
 
-export default Contact
+export default contact
--- kemboi-portfolio/src/components/footer/Footer.jsx
+++ kemboi-portfolio/src/components/footer/Footer.jsx
@@ -13,10 +13,10 @@ const Footer = () => {
 
       <ul className='permalinks'>
         <li><a href="#header">Home</a></li>
-        <li><a href="#About">About</a></li>
-        <li><a href="#Experience">Experience</a></li>
-        <li><a href="#Services">Services</a></li>
-        <li><a href="#Portfolio">Portfolio</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
         <li><a href="#testimonials">Testimonials</a></li>
         <li><a href="#contact">Contact</a></li>
 
@@ -30,6 +30,9 @@ const Footer = () => {
 
       </div>
 
+      <div className="footer__copyright">
+        <small> &copy; BrianBravoski. All rights reserved.</small>
+      </div>
     
     </footer>
   )
